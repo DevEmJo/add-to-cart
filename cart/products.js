023539a1e84d1fdbcf70c3e8eaeb7529008d69bb
DevEmJo <emmanuelnjike237@gmail.com>
2024-08-09 12:06:08 +0200

    Add files via upload

---
+++ cart/products.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        "id": 1,
+        "name": "LD01 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/earpiece.jpg"
+    },
+    {
+        "id": 2,
+        "name": "LD02 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image":  "image/earpiece.jpg"
+    },
+    {
+        "id": 3,
+        "name": "LD03 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/red kask.jpg"
+    },
+    {
+        "id": 4,
+        "name": "LD05 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/airpods black.jpg"
+    },
+    {
+        "id": 5,
+        "name": "LD04 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/speaker on water.jpg"
+    },
+    {
+        "id": 6,
+        "name": "LD05 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/airpods black.jpg"
+    },
+    {
+        "id": 7,
+        "name": "LD05 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/airpods black.jpg"
+    },
+    {
+        "id": 8,
+        "name": "LD05 LOUNGHE CHAIR",
+        "price": 200,
+        "description": "bibibçvgv_vhhonopqbvobvopbfofbhrbopbofbozbvobobfozbfzevopabvopbvobvopqvbgibhibi",
+        "image": "image/airpods black.jpg"
+    }
+];
+export default products;
